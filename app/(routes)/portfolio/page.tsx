6262fe6e86c9fa53811493bef20b8d3c55c53e4c
Angel Sebastian Saccone Graneros <133783405+Angeluz88@users.noreply.github.com>
2026-03-13 20:36:43 -0300

    Update page.tsx

--- app/(routes)/portfolio/page.tsx
+++ app/(routes)/portfolio/page.tsx
@@ -15,8 +15,8 @@ const PortfolioPage = () =>{
             <div className="flex flex-col justify-center h-full">
             
                 <h1 className="text-2xl leading-tight text-center md:text-4xl md:mb-5">
-                Mis ultimos 
-                <span className="text-secondary font-bold"> proyectos realizados.</span>
+                My last 
+                <span className="text-secondary font-bold"> Proyects.</span>
                 </h1>
 
                 <div className="relative m-auto z-10 grid justify-items-center max-w-5xl gap-6 mx-auto mt-4 sm:grid-cols-2 md:grid-cols-4">
